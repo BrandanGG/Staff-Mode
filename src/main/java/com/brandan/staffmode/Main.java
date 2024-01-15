df67262026174af14c762c304e8ebbe581cd4f57
BrandanGG <101829241+BrandanGG@users.noreply.github.com>
2024-01-15 07:24:07 -0500

    Adjusting executors for new classes

--- src/main/java/com/brandan/staffmode/Main.java
+++ src/main/java/com/brandan/staffmode/Main.java
@@ -1,13 +1,18 @@
 package com.brandan.staffmode;
-
+import com.brandan.staffmode.commands.Ban;
+import com.brandan.staffmode.commands.Tempban;
+import com.brandan.staffmode.commands.Unban;
+import com.brandan.staffmode.commands.VanishCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getCommand("unban").setExecutor(new Unban());
+        getCommand("ban").setExecutor(new Ban());
+        getCommand("tempban").setExecutor(new Tempban());
+        getCommand("vanish").setExecutor(new VanishCommand());
     }
 
     @Override
